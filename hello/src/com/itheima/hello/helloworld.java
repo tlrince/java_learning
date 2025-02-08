f721415f028921aa1f7748ef24810ebff1f8f3c4
tlrince <tlrincezcy@gmail.com>
2025-02-08 14:54:58 +0800

    测试版本控制

--- hello/src/com/itheima/hello/helloworld.java
+++ hello/src/com/itheima/hello/helloworld.java
@@ -3,5 +3,7 @@ package com.itheima.hello;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("test");
     }
 }
+
