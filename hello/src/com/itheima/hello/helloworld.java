b062925bdf2b1cc6f330a273940a8477fbc60465
tlrince <tlrincezcy@gmail.com>
2025-02-08 15:02:24 +0800

    继续测试

--- hello/src/com/itheima/hello/helloworld.java
+++ hello/src/com/itheima/hello/helloworld.java
@@ -3,5 +3,6 @@ package com.itheima.hello;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
