a5c44ecd62ac3ae00d69204cfdd885ac19634b28
tlrince <tlrincezcy@gmail.com>
2025-02-08 14:58:43 +0800

    Revert "测试版本控制"
    This reverts commit f721415f

--- hello/src/com/itheima/hello/helloworld.java
+++ hello/src/com/itheima/hello/helloworld.java
@@ -3,8 +3,5 @@ package com.itheima.hello;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("test");
-        System.out.println("test");
     }
 }
-
